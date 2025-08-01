ad8eec1f8dafb4c7e27d909b3e88c2d5da68d8ff
MOHD-ADNAN-123 <adnan10002019@gmail.com>
2025-08-01 12:53:39 +0530

    commit

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Chai from "./chai"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <Chai/>
+  </>
 )
