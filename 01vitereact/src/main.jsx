49fa44ac51bf4939db74c6d979b78a1b4d75da24
MOHD-ADNAN-123 <adnan10002019@gmail.com>
2025-08-02 09:56:22 +0530

    Remove node_modules from tracking

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Chai from "./chai"
+/* eslint-disable react-refresh/only-export-components */
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// import Chai from './chai';
+
+function Myapp() {
+  return (
+    <div>
+      <h1>Checking by creating a function here only</h1>
+    </div>
+  );
+}
+
+const reactElement = React.createElement(
+  'a', // tag
+  { href: 'https://google.com', target: '_blank' }, // props
+  'click me to visit google' // content
+);
 
 createRoot(document.getElementById('root')).render(
   <>
-    <Chai/>
+    {reactElement}
+    {/* <Myapp /> can also be rendered here */}
   </>
-)
+);
+
---
+++ customReact/customreact.js
@@ -0,0 +1,38 @@
+function customRender(reactElement,Container){     //customm render function
+
+
+    /*NOT A SUITABLE CODE AS WE HAVE TO WRITE FOR EVERY ATTRIBUTE
+
+    const domElement = document.createElement(
+        reactElement.type
+    )
+    domElement.innerHTML =reactElement.children  
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+    Container.appendChild(domElement)
+    */
+
+
+    //USING LOOP 
+    
+    const domElement=document.createElement(reactElement.type)
+    domElement.innerHTML =reactElement.children
+    for (const prop in reactElement.props) {
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    Container.appendChild(domElement)
+}
+
+// 1. creating a react element
+const reactElement = {
+    type: 'a',                  //tyoe -> anchor tag
+    props: {
+        href: "https://google.com",  //properties
+        target: '_blank'
+    },
+    children: 'click me to visit google'  //content
+}
+
+const mainContainer =document.querySelector("#root")   //selecting container whose id is root
+
+customRender(reactElement, mainContainer)     // calling custom render function
