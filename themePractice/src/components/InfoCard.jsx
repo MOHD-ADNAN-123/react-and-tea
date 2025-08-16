12d9145fd8aadf3c3c17558126ed78b37b3896a5
MOHD-ADNAN-123 <adnan10002019@gmail.com>
2025-08-16 16:29:00 +0530

    commit

--- themePractice/src/components/InfoCard.jsx
+++ themePractice/src/components/InfoCard.jsx
@@ -1,6 +1,6 @@
 export default function InfoCard(props) {
   return (
-    <div className="p-6 rounded-xl shadow-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 transition-colors duration-300">
+    <div className="p-6 rounded-xl shadow-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 transition-colors duration-500 ease-in-out">
       <h2 className="text-2xl font-bold mb-2">React Theme Switcher</h2>
       <p className="text-sm">
         This is a simple card.Made by {props.username} Toggle between <b>Light</b> 🌞 and <b>Dark</b> 🌙
@@ -9,3 +9,4 @@ export default function InfoCard(props) {
     </div>
   );
 }
+ 
